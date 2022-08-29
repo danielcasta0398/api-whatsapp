2569e66246fe5e5084c8b576e6fd6faf9ad5c433
Juan Daniel Castaño <juancastano0398@gmail.com>
2022-08-29 21:50:33 +0200

    solved error in function reconnectSession

--- utils/reconnectSession.js
+++ utils/reconnectSession.js
@@ -1,18 +1,23 @@
-const { default: axios } = require("axios")
-const { Session } = require("../models/sessionModel")
+const { default: axios } = require('axios');
+const { Session } = require('../models/sessionModel');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 
 const reconnectSession = async () => {
+  const session = await Session.findAll({ where: { status: 'online' } });
 
-    const session = await  Session.findAll({ where:{ status:'online' } })
-    
-    if (session) {
-        session.map( data =>{
-            setTimeout( async () => {
-                await axios.post('http://localhost:4000/api/v1/sessionChat', {id:data.dataValues.userId})
-            }, 5000)
-        } )
-    }
-   
-}
+  if (session) {
+    session.map(data => {
+      setTimeout(async () => {
+        await axios.post(
+          process.env.NODE_ENV == 'production'
+            ? 'https://whatsapp-api-atumano.herokuapp.com/api/v1/sessionchat'
+            : 'http://localhost:4000/api/v1/sessionChat',
+          { id: data.dataValues.userId }
+        );
+      }, 5000);
+    });
+  }
+};
 
-module.exports = { reconnectSession }
+module.exports = { reconnectSession };
